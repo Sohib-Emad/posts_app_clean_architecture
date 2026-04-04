c1fa77e532bc3cf9d302961c987e3f03b5811ae2
sohibemad122 <m9749172@gmail.com>
2026-04-04 09:42:46 +0200

    add di and routes folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:clean_architecture_posts_app/core/di/di.dart' as di;
 import 'package:clean_architecture_posts_app/posts_app.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -5,5 +6,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await ScreenUtil.ensureScreenSize();
+  await di.init();
   runApp(PostsApp());
 }
